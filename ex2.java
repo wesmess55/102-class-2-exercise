b45cc4ca2b50ffcb5eb4fad43606aba0d85c127f
wesmess55 <66344929+wesmess55@users.noreply.github.com>
2020-06-14 13:43:28 -0500

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,23 @@
+public class ex2 {
+
+    public static void main(String args[]){
+
+        String job="Truck Driver";
+        String Name ="Wesley Ray";
+        double salary= 75000.00;
+        double Age= 31;
+    
+        
+
+        System.out.println(Name);
+        System.out.println(job);
+        System.out.println(salary);
+        System.out.println(Age);
+
+
+
+    }
+
+
+    
+}
